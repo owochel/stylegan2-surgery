bf94e304dbf593d405e8bce3a4c678f8aafcbe89
Aydao <ayompm@protonmail.com>
2020-03-26 01:13:08 -0400

    Do not normalize latents

--- train_tpu.sh
+++ train_tpu.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-source "$HOME/bin/activate-tf1"
-
 set -e
 
 export NOISY=1
 export DEBUG=1
+export G_LR=0.003
+export D_LR=0.003
 
-export TPU_NAME=grpc://0.tcp.ngrok.io:12547
-cores=8
+config="config-f"
 
-#config="config-a" # StyleGAN 1
-config="config-f" # StyleGAN 2
-
-data_dir=gs://sgappa-multi/stylegan-encoder/datasets
-dataset=animefaces
+data_dir= # gs://your_bucket/your_dataset_dir/your_dataset_name
+dataset= # your_dataset_name
 mirror=false
 metrics=none
 
-export LABEL_SIZE=0
-#export LABEL_SIZE=full # uncomment this if using labels
-export MODEL_DIR=gs://danbooru-euw4a/test/run50-danbooru-512-conditional-subset-128
-export BATCH_PER=4
+export TPU_HOST= # your host name
+export TPU_NAME= # your tpu name 
+cores= # your tpu cores
+export MODEL_DIR= # gs://your_bucket/your_results_dir/your_dataset_name-resolution_size
+export BATCH_PER= # gpu-wise batch size (batch ubset per gpu) 
 export BATCH_SIZE=$(($BATCH_PER * $cores))
-export RESOLUTION=512
+export RESOLUTION= # resolution_size
 
 set -x
-exec python3 -m pdb -c continue run_training.py --data-dir "${data_dir}" --config="${config}" --dataset="${dataset}" --mirror-augment="${mirror}" --metrics="${metrics}" "$@"
+exec python3 -m pdb -c continue run_training.py --num-gpus="${cores}" --data-dir="${data_dir}" --config="${config}" --dataset="${dataset}" --mirror-augment="${mirror}" --metrics="${metrics}" "$@"
--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -263,7 +263,7 @@ def G_mapping(
     mapping_fmaps           = 512,          # Number of activations in the mapping layers.
     mapping_lrmul           = 0.01,         # Learning rate multiplier for the mapping layers.
     mapping_nonlinearity    = 'lrelu',      # Activation function: 'relu', 'lrelu', etc.
-    normalize_latents       = True,         # Normalize latent vectors (Z) before feeding them to the mapping layers?
+    normalize_latents       = False,         # Normalize latent vectors (Z) before feeding them to the mapping layers?
     dtype                   = 'float32',    # Data type to use for activations and outputs.
     **_kwargs):                             # Ignore unrecognized keyword args.
 
@@ -286,7 +286,9 @@ def G_mapping(
     # Normalize latents.
     if normalize_latents:
         with tf.variable_scope('Normalize'):
+            # PixelNorm
             x *= tf.rsqrt(tf.reduce_mean(tf.square(x), axis=1, keepdims=True) + 1e-8)
+
 
     # Mapping layers.
     for layer_idx in range(mapping_layers):
